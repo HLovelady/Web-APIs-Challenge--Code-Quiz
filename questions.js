099b28fe04d0296aa8a69a4b7d6d4f0d3a91ddb2
haylove <hfayelovelady@gmail.com>
2022-10-11 18:10:04 -0500

    final commit

--- questions.js
+++ questions.js
@@ -1,27 +1,16 @@
 // list of all questions, choices, and answers
-var questions = [
-    {
-      title: "Which one is a looping structure in JavaScript?",
-      choices: ["All the below", "For", "While", "do-while loops"],
-      answer: "All the below"
-    },
-    {
-      title: "What are the two basic groups of data types in JavaScript?",
-      choices: [
-        "Primitive and attribute",
-        "Primitive and reference types",
-        "Reference types and attribute",
-        "None of the above"
-      ],
-      answer: "Primitive and reference types"
-    },
-    {
-      title: "Commonly used data types DO NOT include:",
-      choices: ["strings", "booleans", "alerts", "numbers"],
-      answer: "alerts"
-    },
-    {
-      title: "Boolean operators that can be used in JavaScript include:",
+
+function answer() {
+    if(choices === answer){
+        displayMessage("Correct!");
+    }
+    else {
+        displayMessage("Incorrect.")
+    }
+
+var questions = {
+
+      title: "What are the boolean operators that can be used in JavaScript?",
       choices: [
         "'And' Operator &&",
         "'Or' Operator ||",
@@ -31,16 +20,10 @@ var questions = [
       answer: "All the above"
     },
     {
-      title:
-        "Which one of these is not among the three different types of errors in JavaScript?",
-      choices: [
-        "Animation time errors",
-        "Load time errors",
-        "Run time errors",
-        "Logical Errors"
-      ],
-      answer: "Animation time errors"
-    },
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "class", "booleans", "numbers"],
+        answer: "clss"
+      },
     {
       title: "What is the data type of variables in JavaScript?",
       choices: [
@@ -73,23 +56,12 @@ var questions = [
       answer: "quotes"
     },
     {
-      title:
-        "A very useful tool used during development and debugging for printing content to the debugger is:",
-      choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
-      answer: "console.log"
-    },
-    {
-      title: "What is the type of Pop up boxes available in JavaScript?:",
+      title: "What type of Pop-up box is available in JavaScript?:",
       choices: ["Alert", "Confirm", "Prompt", "All the above"],
       answer: "All the above"
     }
-  ];
+};
 
-  function answer() {
-    if(choices === answer){
-        displayMessage("Correct!");
-    }
-    else {
-        displayMessage("Incorrect.")
-    }
-  }
+
+  
+  
--- script.js
+++ script.js
@@ -1,46 +1,55 @@
 // DOM elements
 var questionsEl = document.querySelector("#questions");
+var questiont = document.querySelector("#questiont");
 var timerEl = document.querySelector("#time");
 var choicesEl = document.querySelector("#choices");
+var highscoreEl = document.querySelector("#highscore");
 var submitBtn = document.querySelector("#submit");
 var startBtn = document.querySelector("#start");
 var initialsEl = document.querySelector("#initials");
 var feedbackEl = document.querySelector("#feedback");
 
 
-
-//var message =
-//  '';
-//var words = message.split('');
-
-// Timer that counts down from 60
+// Timer that counts down from 5
 function countdown() {
-  var timeLeft = 60;
-
-  // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
-  var timeInterval = setInterval(function) {
-    // As long as the `timeLeft` is greater than 1
-    if (timeLeft > 1) {
-      // Set the `textContent` of `timerEl` to show the remaining seconds
-      timerEl.textContent = timeLeft;
-      // Decrement `timeLeft` by 1
-      timeLeft--;
-    } else if (timeLeft === 1) {
-      // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
-      timerEl.textContent = timeLeft;
-      timeLeft--;
-    } else {
-      // Once `timeLeft` gets to 0, set `timerEl` to an empty string
-      timerEl.textContent = '';
-      // Use `clearInterval()` to stop the timer
-      clearInterval(timeInterval);
-      // Call the `displayMessage()` function
-      displayMessage();
-    }
-  }, 1000);
+    var timeLeft = 60;
+  
+    // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+    var timeInterval = setInterval(function () {
+      // As long as the `timeLeft` is greater than 1
+      if (timeLeft > 1) {
+        // Set the `textContent` of `timerEl` to show the remaining seconds
+        timerEl.textContent = timeLeft;
+        // Decrement `timeLeft` by 1
+        timeLeft--;
+      } else if (timeLeft === 1) {
+        // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
+        timerEl.textContent = timeLeft;
+        timeLeft--;
+      } else {
+        // Once `timeLeft` gets to 0, set `timerEl` to an empty string
+        timerEl.textContent = '';
+        // Use `clearInterval()` to stop the timer
+        clearInterval(timeInterval);
+        // Call the `displayMessage()` function
+        displayMessage();
+      }
+    }, 1000);
+  }
+  countdown();
+ 
+function displayMessage() {
+    timeEl.textContent = " ";
+    var endEl = querySelector("end");
+    endEl.setAttribute("#end");
+    mainEl.appendChild(endEl);
 }
 
-countdown();
 
-
-
+// Clicking on next button displays next image in carousel
+  highscoreEl.addEventListener("click", function(event) {
+    // Stops event from bubbling up and new window opening
+    event.stopPropagation();
+  
+    navigate(1);
+  });
